26e25c753f2613512963ae2b1ab6d2dfd7957a23
TLKT0M <tom.klein@student.uni-siegen.de>
2022-03-29 14:28:24 +0200

    Besprechung Michael

--- src/RobustOptimization.jl
+++ src/RobustOptimization.jl
@@ -1,13 +1,20 @@
 module RobustOptimization
-
+import Pkg
+Pkg.add("JuMP")
+Pkg.add("HiGHS")
+using JuMP
+using HiGHS
 testfunc() = 0
 function get_robust(m::Model)
-    if haskey(m.ext, :JuMPeR)
-        return m.ext[:JuMPeR]
-    end
-    error("This functionality is only available for a JuMPeR RobustModel.")
+    m.ext:JuMPeR
+    #print(m.ext[:JuMPeR])
+    print("EXT: ",m.ext)
+    #if haskey(m.ext, :JuMPeR)
+    #    return m.ext[:JuMPeR]
+    #end
+    #error("This functionality is only available for a JuMPeR RobustModel.")
 end
-
-
+m = Model()
+get_robust(m)
 include("uncertain.jl")
 end
