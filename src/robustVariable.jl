5c47c53366f00de4085dc6e720dae500ec9589dd
TLKT0M <tom.klein@student.uni-siegen.de>
2022-04-03 16:20:10 +0200

    Katastrophe

--- src/robustVariable.jl
+++ src/robustVariable.jl
@@ -1,32 +1,19 @@
 import JuMP
 
-struct AddTwice
-    info::JuMP.VariableInfo
-end
-function JuMP.build_variable(
-    _err::Function,
-    info::JuMP.VariableInfo,
-    ::Type{AddTwice};
-    kwargs...
-)
-    println("Can also use $kwargs here.")
-    return AddTwice(info)
+
+struct robustVariable
+    test:Int16
 end
 
-function JuMP.add_variable(
-           model::JuMP.Model,
-           duplicate::AddTwice,
-           name::String,
-       )
-           a = JuMP.add_variable(
-               model,
-               JuMP.ScalarVariable(duplicate.info),
-               name * "_a",
-            )
-           b = JuMP.add_variable(
-               model,
-               JuMP.ScalarVariable(duplicate.info),
-               name * "_b",
-            )
-           return (a, b)
-       end
+function JuMP.build_variable(_error::Function, ::Type{robustVariable})
+    return robustVariable()
+end
+
+function JuMP.add_variable(m::JuMP.Model, v::robustVariable, name::String="")
+    vari = JuMP.add_variable(
+        m,
+        v,
+        name
+    )
+    return vari
+end
--- src/test_vars.jl
+++ src/test_vars.jl
@@ -1,2 +1,8 @@
-impor
+import Pkg
+Pkg.add("JuMP")
+using JuMP
+include("robustVariable.jl")
 model = Model();
+
+
+@variable(model, test=10, name="test")
